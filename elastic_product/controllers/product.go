73a558ac820706d666ad550d89bf6035ced458ee
Indra Octama <omyank2007i@gmail.com>
2019-07-17 16:13:58 +0700

    elastic product

--- elastic_product/controllers/product.go
+++ elastic_product/controllers/product.go
@@ -1,1 +1,25 @@
 package controllers
+
+import (
+	"github.com/astaxie/beego"
+	"github.com/indroct/go-for-beginner/elastic_product/models"
+)
+
+type ProductController struct {
+	beego.Controller
+}
+
+// @router / [get]
+func(this *ProductController) GetProductData(){
+
+	products,err := models.GetProducts()
+	
+	if err != nil{
+		this.Ctx.Output.SetStatus(401)
+		this.Data["json"] = err
+	}else{
+		this.Ctx.Output.SetStatus(200)
+		this.Data["json"] = products
+	}
+	this.ServeJSON()
+}
